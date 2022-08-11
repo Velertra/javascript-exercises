8ad119888fc6d46890b3915725085ec952f4fdca
Velertra <velertra43@gmail.com>
2022-08-11 00:26:28 -0700

    odin practice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+//const repeatString = function(string, num){
+   // let string = 'hey'
+  //  for (let i = 0; i <= 3; i++) {
+        
+  //      return string * i;
+ //       }
+ //   }
+        
+    
 
-};
+
+ const repeatString = function(str, num){
+    let expression = '';
+    while (num > 0) {
+        expression += str;
+        num--;
+    }
+    return expression;
+    }
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let expression = '';
+    let newString = str.split("")
+    let newerString = newString.reverse();
+    str = newerString.join('');
+    expression += str;
+    return expression;
 };
 
 // Do not edit below this line
