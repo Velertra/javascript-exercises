cf560296743a56a496c27809999b21b35f7d117f
Velertra <velertra43@gmail.com>
2022-08-12 13:46:42 -0700

    practice odin

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,27 +1,9 @@
 const sumAll = function(a, b) {
-    let sum = 0;
-    if (a >  b) {
-        let arr = new Array(a - b + 1);
-        console.log(arr);
-        for (let i = 0; i  < arr.length; i++, a --) {
-            arr[i] = a;
-            for (k = 0; k < arr.length; k++) {
-                sum += arr[i];
-           }
-        }
-        return sum;
-    } else {
-        let arro = new Array(b - a + 1);
-
-        for (let j = 0; j < arro.length; j++, a++) {
-            arro[j] = a;
-            for (k = 0; k < arro.length; k++) {
-                sum += arro[j];
-           }
-        }
-        return sum;
-    }
     
+    if(a==b) return a;
+    var sum = 0;
+    for(i=((a<b)?a:b);i<=((a<b)?b:a);i++)sum+=i;
+    return sum;
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,29 @@
-const ftoc = function() {
+const ftoc = function(fahr) {
+  let finalNum = 0;
+   let newFtoc = (fahr - 32) * (5/9);
+  
+  
+  if(Math.round(newFtoc) !== newFtoc) {
+    newFtoc = newFtoc.toFixed(1);
+    }
+
+
+  finalSum = Number(newFtoc);
+    return finalSum;
 
 };
 
-const ctof = function() {
+const ctof = function(ctof) {
+  let finalNum = 0;
+   let newFtoc = (ctof * 9/5 );
+  let newNew = newFtoc + 32;
+   if(Math.round(newNew) !== newNew) {
+    newNew = newNew.toFixed(1); 
+    
+  }
+  
+  finalSum = Number(newNew);
+    return finalSum;
 
 };
 
