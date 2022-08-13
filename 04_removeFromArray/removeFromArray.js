f312c52ba3fb674d99d1fa9b084288d0b47d822c
Velertra <velertra43@gmail.com>
2022-08-13 08:31:09 -0700

    never committed these finsihed lessons

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+//const removeFromArray = function(array, ...num) {
+    //    let deleteCount = 1;
+    //    let start = array.indexOf(num);
+    //    let newArray = array.splice(start, deleteCount);
+     //   return array;
+//};
 
+function removeFromArray(array, ...args) {
+    return array.filter(x => !args.includes(x))
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
